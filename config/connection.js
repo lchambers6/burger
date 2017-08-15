9f931dc3cb94123c695c29879176172a6d47e04c
Luke Chambers <lukechambers91@gmail.com>
2017-08-14 20:36:23 -0400

    test failed reverting back

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSBD_URLm) {
+if (process.env.JAWSBD_URL) {
   connection = mysql.createConnection(process.env.JAWSBD_URL);
 } else {
   connection = mysql.createConnection({
